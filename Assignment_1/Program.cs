2695dab076e55e643cc20fbd0bd813e347605053
Menna Saber <mennasaper@gmail.com>
2026-08-29 10:20:29 +0300

    Question1

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        class Book
+        {
+            public string Title;
+            public int Pages;
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //#region Question1
+            ////1. Create a Book class with a Title (string) and Pages (int). Create a Book object and store it
+            ////in a variable of type object.Print it.
+
+            //Book b1 = new Book();
+            //object obj = b1;
+            //Console.WriteLine(obj);
+
+            //#endregion
+
+
+
+
+
+
         }
+
+
     }
 }
